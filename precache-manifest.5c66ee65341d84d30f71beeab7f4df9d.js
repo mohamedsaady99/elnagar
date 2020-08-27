5d162048c42c3737cad858b281e8ee1190e0b6f1
mohamedsaady99 <33896923+mohamedsaady99@users.noreply.github.com>
2020-08-27 12:29:09 +0200

    Updates

---
+++ precache-manifest.5c66ee65341d84d30f71beeab7f4df9d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8c87607139d359830d6fbb62f56138",
+    "url": "/elnagar/index.html"
+  },
+  {
+    "revision": "8689115f6437b05becc5",
+    "url": "/elnagar/static/css/2.98ca0a0f.chunk.css"
+  },
+  {
+    "revision": "bc6daffe69231359d71d",
+    "url": "/elnagar/static/css/main.aef2f23b.chunk.css"
+  },
+  {
+    "revision": "8689115f6437b05becc5",
+    "url": "/elnagar/static/js/2.a43feaf3.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/elnagar/static/js/2.a43feaf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6daffe69231359d71d",
+    "url": "/elnagar/static/js/main.79ff3d54.chunk.js"
+  },
+  {
+    "revision": "ce117163b4fce7dd235b",
+    "url": "/elnagar/static/js/runtime-main.1a2638c0.js"
+  },
+  {
+    "revision": "0fdf4c84ee9f64755c7789f5a628be69",
+    "url": "/elnagar/static/media/Onassis.0fdf4c84.png"
+  },
+  {
+    "revision": "c22ae405b0aaa87363ddef2d35ff2f35",
+    "url": "/elnagar/static/media/Portfolio.c22ae405.jpg"
+  },
+  {
+    "revision": "81a8ad5fc4b73e4e4e9f3cec925799b8",
+    "url": "/elnagar/static/media/create.81a8ad5f.jpg"
+  },
+  {
+    "revision": "0a7828f066b04b51a8cffa89524cbcc3",
+    "url": "/elnagar/static/media/dstt.0a7828f0.png"
+  },
+  {
+    "revision": "bb167736db4e7c0e7635b65edf69c596",
+    "url": "/elnagar/static/media/ecommerce.bb167736.png"
+  },
+  {
+    "revision": "83531e51ea1a76d9839cbf8249a20f50",
+    "url": "/elnagar/static/media/galaxy.83531e51.jpg"
+  },
+  {
+    "revision": "6b1d7b9249e7cdf3d86b567d3032115a",
+    "url": "/elnagar/static/media/home-bg.6b1d7b92.jpg"
+  },
+  {
+    "revision": "906f6ca9af9f6bcb76c4a6796d6314dc",
+    "url": "/elnagar/static/media/logo.906f6ca9.png"
+  },
+  {
+    "revision": "1e6a5843e3bb376e9068f2bd4c8f36b4",
+    "url": "/elnagar/static/media/me.1e6a5843.jpg"
+  },
+  {
+    "revision": "417f539ca6ca76aa22769ecde853d0b1",
+    "url": "/elnagar/static/media/rima.417f539c.jpg"
+  },
+  {
+    "revision": "30f4ef95421d89319d4939c9e586a9a8",
+    "url": "/elnagar/static/media/roofing.30f4ef95.jpg"
+  }
+]);
